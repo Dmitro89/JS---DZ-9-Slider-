7de4e05f0fd1846eba4e76229deddeeca0a6ed97
Dmitro89 <akimenko_dmitro@mail.ru>
2017-05-21 00:11:39 +0300

    JS - DZ#9

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.sl-slide-show').slick({
+  	infinite: true,
+    slidesToShow: 3,
+  	slidesToScroll: 1,
+  	centermode:true,
+  	infinite:true,
+  	autoplay: true	
+  });
+});
